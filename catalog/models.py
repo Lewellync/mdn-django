3ee0b67dde7245cdaf6946d433df572b30412475
Lewellync <lewellync@gmail.com>
2019-04-26 16:26:59 -0400

    part 4: work and challenge

--- catalog/models.py
+++ catalog/models.py
@@ -20,6 +20,7 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -36,6 +37,13 @@ class Book(models.Model):
     genre = models.ManyToManyField('Genre', help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
